f2797600595a6c3599534c5ce00385e9ea400aeb
Tyler Gaw <me@tylergaw.com>
2018-01-03 22:41:40 -0500

    Shave the yak (#23)
    * Tearing out old stuff, starting new build setup
    
    * Moves all posts to directory layout
    
    * Removing more old crud not needed anymore
    
    * Shoehorns in a check for the layout template
    
    * Replacement for @apply
    
    * Removes apply and matches
    
    * Fixes home hover border
    
    * Puts data-page attribute back in place
    
    * Fixes date handling on posts
    
    * I dunno, fixes stuff
    
    * Removes more :matches
    
    * Dont specify timezone in dateFormat helper
    
    * Updates color function uses in blog post
    
    * V SOPHISTICATED DEPLOY STRAT FOR 2018

---
+++ scripts/build.js
@@ -0,0 +1,67 @@
+const metalsmith = require('metalsmith');
+const msIf = require('metalsmith-if');
+const browserSync = require('metalsmith-browser-sync');
+const inplace = require('metalsmith-in-place');
+const layouts = require('metalsmith-layouts');
+const postcss = require('metalsmith-postcss');
+
+const handlebars = require('handlebars');
+const helpers = require('./handlebars-helpers');
+
+const shouldServe = process.env.SERVE === 'true';
+
+// Register all our helpers at once.
+// See ./handlebars-helpers.js for docs.
+handlebars.registerHelper(helpers);
+
+metalsmith(__dirname)
+  .source('../src')
+  .destination('../build')
+  .metadata({
+    site: {
+      url: shouldServe ? '' : 'https://tylergaw.com'
+    }
+  })
+  .use(
+    postcss({
+      plugins: {
+        'postcss-import': {},
+        'postcss-nesting': {},
+        'postcss-extend-rule': {}
+      }
+    })
+  )
+  .ignore('**/modules/*.css')
+  .use(
+    inplace({
+      engine: 'handlebars',
+      pattern: ['**/*.html'],
+      partials: '../partials'
+    })
+  )
+  .use(
+    layouts({
+      engine: 'handlebars',
+      default: 'default.html',
+      pattern: ['**/*.html'],
+      directory: '../layouts',
+      partials: '../partials'
+    })
+  )
+  .use(
+    msIf(
+      shouldServe,
+      browserSync({
+        port: 8001,
+        server: {
+          baseDir: 'build'
+        },
+        files: ['src/**/*', 'layouts/**/*', 'partials/**/*']
+      })
+    )
+  )
+  .build(err => {
+    if (err) {
+      console.log(err);
+    }
+  })
