19d01840ca3e8fc86b5ccadd9099908fa2103a46
Tyler Gaw <me@tylergaw.com>
2020-05-04 21:24:49 -0400

    Way too much work for a single commit

--- scripts/build.js
+++ scripts/build.js
@@ -1,25 +1,25 @@
-const metalsmith = require('metalsmith');
-const msIf = require('metalsmith-if');
-const browserSync = require('metalsmith-browser-sync');
-const inplace = require('metalsmith-in-place');
-const layouts = require('metalsmith-layouts');
-const postcss = require('metalsmith-postcss');
+const metalsmith = require("metalsmith");
+const msIf = require("metalsmith-if");
+const browserSync = require("metalsmith-browser-sync");
+const inplace = require("metalsmith-in-place");
+const layouts = require("metalsmith-layouts");
+const postcss = require("metalsmith-postcss");
 
-const handlebars = require('handlebars');
-const helpers = require('./handlebars-helpers');
+const handlebars = require("handlebars");
+const helpers = require("./handlebars-helpers");
 
-const shouldServe = process.env.SERVE === 'true';
-const watch = process.env.WATCH === 'true';
+const shouldServe = process.env.SERVE === "true";
+const watch = process.env.WATCH === "true";
 
 let browserSyncOpts = {
   port: 8001,
   server: {
-    baseDir: 'build'
+    baseDir: "build"
   }
 };
 
 if (watch) {
-  browserSyncOpts.files = ['src/**/*', 'layouts/**/*', 'partials/**/*']
+  browserSyncOpts.files = ["src/**/*", "layouts/**/*", "partials/**/*"];
 }
 
 // Register all our helpers at once.
@@ -27,54 +27,50 @@ if (watch) {
 handlebars.registerHelper(helpers);
 
 metalsmith(__dirname)
-  .source('../src')
-  .destination('../build')
+  .source("../src")
+  .destination("../build")
   .metadata({
     site: {
-      url: shouldServe ? '' : 'https://tylergaw.com'
-    }
+      url: shouldServe ? "" : "https://tylergaw.com"
+    },
+    year: new Date().getFullYear()
   })
   .use(
     postcss({
       plugins: {
-        'postcss-import': {},
-        'postcss-nesting': {}
+        "postcss-import": {},
+        "postcss-nesting": {}
       }
     })
   )
-  .ignore('**/modules/*.css')
+  .ignore("**/modules/*.css")
   .use(
     inplace({
-      engine: 'handlebars',
-      pattern: ['**/*.html'],
-      partials: '../partials'
+      engine: "handlebars",
+      pattern: ["**/*.html"],
+      partials: "../partials"
     })
   )
   .use(
     inplace({
-      engine: 'handlebars',
-      pattern: ['**/*.css'],
-      partials: '../src/css'
+      engine: "handlebars",
+      pattern: ["**/*.css"],
+      partials: "../src/css"
     })
   )
   .use(
     layouts({
-      engine: 'handlebars',
-      default: 'default.html',
-      pattern: ['**/*.html'],
-      directory: '../layouts',
-      partials: '../partials'
+      engine: "handlebars",
+      default: "default.html",
+      pattern: ["**/*.html"],
+      directory: "../layouts",
+      partials: "../partials"
     })
   )
-  .ignore('**/tylergaw-critical.css')
-  .use(
-    msIf(
-      shouldServe,
-      browserSync(browserSyncOpts)
-    )
-  )
-  .build(err => {
+  .ignore("**/tylergaw-critical.css")
+  .use(msIf(shouldServe, browserSync(browserSyncOpts)))
+  .build((err) => {
     if (err) {
       console.log(err);
     }
-  })
+  });
