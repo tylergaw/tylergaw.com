e6bc331394c25c8666b264cc2049a3adfc533e06
Tyler Gaw <me@tylergaw.com>
2020-07-19 16:22:00 -0400

    Adds foundation build tooling setup

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = function (conf) {
+  conf.setTemplateFormats([
+    "njk",
+    "gif",
+    "jpg",
+    "png",
+    "webp",
+    "svg",
+    "woff2",
+    "ico",
+    "js",
+  ]);
+  conf.addPassthroughCopy("./src/manifest.webmanifest");
+  conf.addPassthroughCopy("./src/keybase.txt");
+  conf.addPassthroughCopy("./src/rss.xml");
+
+  conf.addWatchTarget("./src/**/*.css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
