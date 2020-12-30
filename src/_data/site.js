b9c820b7405bd453fd94b344c2523c1b737bf2e0
Tyler Gaw <me@tylergaw.com>
2020-12-30 00:01:05 -0500

    Updates versions section of about

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,3 +1,11 @@
 module.exports = {
   environment: process.env.ELEVENTY_ENV,
+  versions: [
+    "2018–2020",
+    "2014–2018",
+    "2011–2014",
+    "2009–2011",
+    "2007–2009",
+    "2006–2007",
+  ],
 };
