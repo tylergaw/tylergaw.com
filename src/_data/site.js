744898897b9b47d34059a135fde1cd3396dbd4d2
Tyler Gaw <me@tylergaw.com>
2021-01-24 18:18:29 -0500

    Adds site.url data, fixes meta

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,19 @@
+const getSiteUrl = () => {
+  if (process.env.ELEVENTY_ENV === "local") {
+    return "http://localhost:8080";
+  }
+
+  if (process.env.ELEVENTY_ENV === "production") {
+    return process.env.CONTEXT === "production"
+      ? process.env.URL
+      : process.env.DEPLOY_PRIME_URL;
+  }
+};
+
 module.exports = {
   environment: process.env.ELEVENTY_ENV,
   year: new Date().getFullYear(),
+  url: getSiteUrl(),
   versions: [
     "2018–2020",
     "2014–2018",
